99880687e02d6a9ef353e93b3f3bcfdce5f5d4e7
Alex Suess <alex@envivo.de>
2019-05-15 10:05:38 +0200

    header bg, footer

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,10 +1,27 @@
 const menu = document.getElementById("header-menu-list");
 const btn = document.getElementById("toggle-menu-button");
 const icon = document.getElementById("menu-icon");
+const body = document.getElementById("body");
+const headerBg = document.getElementById('headerBg')
+
+
 btn.addEventListener("click", toggleMenu);
+
+window.onscroll = function() {displayHeaderBg()};
 
 function toggleMenu() {
 	menu.classList.toggle("display");
 	btn.classList.toggle("active");
 	icon.classList.toggle("fa-rotate-90");
+}
+
+function displayHeaderBg() {
+	var scrollTop = window.scrollY;
+	if (scrollTop > 0) {
+		headerBg.classList.add("display");	
+	}
+	else {
+		headerBg.classList.remove("display");
+	}
+	
 }
