acabea250a410dc9ff053197c827190366a405bb
Alex Suess <alex@envivo.de>
2019-05-15 09:39:40 +0200

    some changes

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,8 +1,10 @@
 const menu = document.getElementById("header-menu-list");
 const btn = document.getElementById("toggle-menu-button");
+const icon = document.getElementById("menu-icon");
 btn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
 	menu.classList.toggle("display");
 	btn.classList.toggle("active");
+	icon.classList.toggle("fa-rotate-90");
 }
