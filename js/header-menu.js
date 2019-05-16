77072426760a34e4d43038db05e8fdee80109961
Alex Suess <alex@envivo.de>
2019-05-16 16:57:34 +0200

    include js etc

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,27 +1,35 @@
-const menu = document.getElementById("header-menu-list");
-const btn = document.getElementById("toggle-menu-button");
-const icon = document.getElementById("menu-icon");
-const body = document.getElementById("body");
-const headerBg = document.getElementById('headerBg')
+includeHTML();
 
+window.onload = function () {
+	const menu = document.getElementById("header-menu-list");
+	const btn = document.getElementById("toggle-menu-button");
+	const icon = document.getElementById("menu-icon");
+	const body = document.getElementById("body");
+	const headerBg = document.getElementById('headerBg');
+	
+	if (btn) {
+		btn.addEventListener("click", toggleMenu);
+	}	
 
-btn.addEventListener("click", toggleMenu);
-
-window.onscroll = function() {displayHeaderBg()};
-
-function toggleMenu() {
-	menu.classList.toggle("display");
-	btn.classList.toggle("active");
-	icon.classList.toggle("fa-rotate-90");
-}
-
-function displayHeaderBg() {
-	var scrollTop = window.scrollY;
-	if (scrollTop > 0) {
-		headerBg.classList.add("display");	
+	function toggleMenu() {
+		console.log("asdas");
+		menu.classList.toggle("display");
+		btn.classList.toggle("active");
+		icon.classList.toggle("fa-rotate-90");
 	}
-	else {
-		headerBg.classList.remove("display");
-	}
+
 	
 }
+function displayHeaderBg() {
+		var scrollTop = window.scrollY;
+		if (scrollTop > 0) {
+			headerBg.classList.add("display");	
+		}
+		else {
+			headerBg.classList.remove("display");
+		}
+		
+	}
+	
+window.onscroll = function() {displayHeaderBg()};
+
