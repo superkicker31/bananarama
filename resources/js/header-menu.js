fd31ee4eebc34ebeffd292b8180ba9fabb29c868
Alex Suess <alex@envivo.de>
2019-08-28 17:33:25 +0200

    test js

--- resources/js/header-menu.js
+++ resources/js/header-menu.js
@@ -1,23 +1,23 @@
 includeHTML();
-
 window.onload = function () {
-    console.log("js running");
-    const btn = document.getElementById("toggle-menu-button");
-    const menu = document.getElementById("header-menu-list");
-    const icon = document.getElementById("menu-icon");
-    const header = document.getElementById('header');
-    const headerBg = document.getElementById('headerBg');
+    setTimeout(function () {
+        const btn = document.getElementById("toggle-menu-button");
+        const menu = document.getElementById("header-menu-list");
+        const icon = document.getElementById("menu-icon");
+        const header = document.getElementById("header");
+        const headerBg = document.getElementById("headerBg");
 
-    if (btn) {
-        btn.addEventListener("click", toggleMenu);
-    }
-
-    function toggleMenu() {
-        menu.classList.toggle("display");
-        btn.classList.toggle("active");
-        icon.classList.toggle("fa-rotate-90");
-    }
+        if (btn) {
+            btn.addEventListener("click", toggleMenu);
+        }
+    },100);
 };
+
+function toggleMenu() {
+    menu.classList.toggle("display");
+    btn.classList.toggle("active");
+    icon.classList.toggle("fa-rotate-90");
+}
 
 function displayHeaderBg() {
 	let headerHeight = header.offsetHeight;
