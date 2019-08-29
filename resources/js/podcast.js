d46deba20ac620ac7ba474e38e5a9812d8ec30b5
Alex Suess <alex@envivo.de>
2019-08-29 16:16:34 +0200

    review page added

--- resources/js/podcast.js
+++ resources/js/podcast.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 window.onload = function () {
 
     const dynastyEpisodes = [
@@ -10,7 +5,9 @@ window.onload = function () {
     ];
 
     const dynastyContainer = document.getElementById('dynasty-pod-container');
-    dynastyEpisodes.forEach(function (episode){
-        dynastyContainer.append("<iframe src='https://podomatic.com/embed/html5/episode/" + episode + "?autoplay=false' height='120' width='100%'frameborder='0' marginheight='0' marginwidth='0' scrolling='no' allowfullscreen>")
+    dynastyEpisodes.forEach(function (episode) {
+        var div = document.createElement('div');
+        div.innerHTML = "<iframe src='https://podomatic.com/embed/html5/episode/" + episode + "?autoplay=false' height='120' width='100%'frameborder='0' marginheight='0' marginwidth='0' scrolling='no' allowfullscreen>";
+        dynastyContainer.append(div);
     });
 };
---
+++ resources/js/slider.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.review-slider').slick({
+        prevArrow: "<button type='button' class='slick-prev'><i class='fa fa-chevron-left fa-2x'></i></button>",
+        nextArrow: "<button type='button' class='slick-prev'><i class='fa fa-chevron-right fa-2x'></i></button>"
+    });
+});
